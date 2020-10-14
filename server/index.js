b12d7bffb573da8b999313c629908419a4929fd8
Michael Tobias <michaelremitobias@gmail.com>
2020-10-13 17:40:49 -0700

    Added find all GET request

--- server/index.js
+++ server/index.js
@@ -10,6 +10,16 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../client/build')))
 
+app.get('/moist-air/game', (req, res) => {
+  Game.find({}).exec((err, results) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(results)
+    }
+  })
+})
+
 app.get('/moist-air/game',(req, res)=>{
   reqId = req.query.id;
   let data = Game.find({'id': reqId});
@@ -21,6 +31,45 @@ app.get('/moist-air/game',(req, res)=>{
       res.send(gameData);
     }
   })
+})
+
+app.post('/moist-air/game', (req, res) => {
+  Game.insertOne({
+    {
+      id: req.body.id,
+      titleCover: req.body.titleCover,
+      title: req.body.title,
+      price: req.body.price,
+      aboutInfo: req.body.aboutInfo,
+      requirements: {
+        os: {type: String},
+        processor: {type: String},
+        memory: {type: String},
+        graphics: {type: String},
+        directX: {type: String},
+        storage: {type: String},
+        },
+      genre: [{type: String}],
+      developer: {type: String},
+      publisher: {type: String},
+      releaseDate: {type: String},
+      steamAcheivments:[{type: String}],
+      languages: [{
+        languageName: {type: String},
+        interface: {type: Boolean},
+        fullAudio:  {type: Boolean},
+        subtitles:  {type: Boolean},
+        }],
+      attributes:{
+        achievements:  {type: Boolean},
+        controllerSupport:  {type: Boolean},
+        partialControllersupport:  {type: Boolean},
+        remotePlay: {type: Boolean}
+        },
+      moreLikeThis: [{titleImage:{type: String}, price: {type: String}}]
+    }
+  })
+  res.send(console.log('it is working'))
 })
 
 let server = app.listen(port, () => {
