682b4bd37e659a5494cb515371ee6843b53cae1d
Michael Tobias <michaelremitobias@gmail.com>
2020-10-15 11:25:35 -0700

    Add mongo functions file

---
+++ db/mongoDBFunctions.js
@@ -0,0 +1,23 @@
+const Game = require('../db/index.js');
+
+
+//get all
+const getAllGames =
+
+
+//get 1 game by 'id'
+
+
+
+//post 1 neew game
+
+
+
+//update(put) game with new data
+
+
+//delete 1 game
+
+
+
+module.exports
