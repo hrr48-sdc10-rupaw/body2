d533855b47c77a402af7edd52df997f9ea79c17e
Michael Tobias <michaelremitobias@gmail.com>
2020-10-15 12:11:35 -0700

    Mongo seperated

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 var cors = require('cors')
 const path = require('path')
 const port = 3000;
-const Game = require('../db/index.js');
 
 app.use(cors())
 app.use(express.json());
@@ -15,81 +14,15 @@ app.use(express.static(path.join(__dirname, '../client/build')))
 //mongoDB
 const db = require('../db/mongoDBFunctions.js')
 
-app.get('/moist-air/', db.getAllGames)
+app.get('/moist-air/', db.getAllGames);
 
-app.get('/moist-air/game',(req, res)=>{
-  reqId = req.query.id;
-  let data = Game.find({'id': reqId});
+app.get('/moist-air/game', db.getOneGame);
 
-  data.exec((err, gameData)=>{
-    if(err){
-      res.send('there was an error');
-    } else {
-      res.send(gameData);
-    }
-  })
-})
+app.post('/moist-air/', db.postGame);
 
-app.post('/moist-air/', (req, res) => {
-  Game.create({
-      id: req.body.id,
-      titleCover: req.body.titleCover,
-      title: req.body.title,
-      price: req.body.price,
-      aboutInfo: req.body.aboutInfo,
-      requirements: {
-        os: req.body.requirements.os,
-        processor: req.body.requirements.processor,
-        memory: req.body.requirements.memory,
-        graphics: req.body.requirements.graphics,
-        directX: req.body.requirements.directX,
-        storage: req.body.requirements.storage,
-        },
-      genre: req.body.genre,
-      developer: req.body.developer,
-      publisher: req.body.publisher,
-      releaseDate: req.body.releaseDate,
-      steamAcheivments: req.body.steamAcheivments,
-      languages: req.body.languages,
-      attributes: {
-        achievements:  req.body.attributes.achievements,
-        controllerSupport:  req.body.attributes.controllerSupport,
-        partialControllersupport:  req.body.attributes.partialControllersupport,
-        remotePlay: req.body.attributes.remotePlay
-        },
-      moreLikeThis: req.body.moreLikeThis
-  }, (err) => {
-    if (err) {
-      throw err;
-    } else {
-      res.writeHead(201);
-      res.end('New Game Created')
-    }
-  })
-});
+app.put('/moist-air/game/', db.updateGame);
 
-app.put('/moist-air/game/', (req, res) => {
-  req.query.id = parseInt(req.query.id);
-  Game.findOneAndUpdate({id: req.query.id}, {price: req.body.price}, (err, result ) => {
-    if (err) {
-      throw err;
-    } else {
-      res.send(result)
-      console.log('Game Updated')
-    }
-  })
-});
-
-app.delete('/moist-air/game', (req, res) => {
-  req.query.id = parseInt(req.query.id);
-  Game.deleteOne({id: req.query.id}, (err) => {
-    if (err) {
-      throw err;
-    } else {
-      res.end('Game Deleted')
-    }
-  })
-});
+app.delete('/moist-air/game', db.deleteOneGame);
 
 let server = app.listen(port, () => {
   console.log(`listening at ${port}...`);
